ac3bf755528b60472d5498299ff543349c6808c5
plotier <plotier15@gmail.com>
2021-08-03 19:15:50 +0000

    right col

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,7 +7,7 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="container-flex">
+		<div className="container-fluid">
 			<div style={{ height: "450px" }}>
 				<div className="row pic m-auto">
 					<div className="rectangle">
@@ -24,7 +24,21 @@ export const Home = () => {
 			</div>
 			<div className="row">
 				<div className="col p-3 mb-2 bg-secondary text-white">1 of 2</div>
-				<div className="col p-3 mb-2 bg-success text-white">2 of 2</div>
+
+				<div className="col p-1 mb-2  text-white">
+					<div className="card" style={{ width: "100%" }}>
+						<img className="card-img-top" src={recorte} alt="Card image cap" />
+					</div>
+					<div className="card" style={{ width: "100%" }}>
+						<img className="card-img-top" src={recorte} alt="Card image cap" />
+					</div>
+					<div className="card" style={{ width: "100%" }}>
+						<img className="card-img-top" src={recorte} alt="Card image cap" />
+					</div>
+					<div className="card" style={{ width: "100%" }}>
+						<img className="card-img-top" src={recorte} alt="Card image cap" />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
