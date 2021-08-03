7c925a76eda9d8d4b4bd93d6f8e187c4c4ee4340
plotier <plotier15@gmail.com>
2021-08-03 14:37:36 +0000

    first pic

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,24 +1,21 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import recorte from "../../img/recorte.jpg";
 import "../../styles/home.scss";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo! home branch</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+		<div className="container-flex">
+			<div className="row">
+				<img src={recorte} className="pic m-auto " />
+				rectangle
+			</div>
+			<div className="row">
+				<div className="col p-3 mb-2 bg-secondary text-white">1 of 2</div>
+				<div className="col p-3 mb-2 bg-success text-white">2 of 2</div>
+			</div>
 		</div>
 	);
 };
