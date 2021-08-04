f0ae48c62186494879023af556b8694bf3aca706
plotier <plotier15@gmail.com>
2021-08-04 10:40:58 +0000

    search for items

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import recorte from "../../img/recorte.jpg";
+import maps from "../../img/maps.png";
 import "../../styles/home.scss";
 
 export const Home = () => {
@@ -24,20 +25,88 @@ export const Home = () => {
 			</div>
 			<div className="row">
 				<div className="col-3 p-1 mb-2 bg-secondary text-white filter">
-					<h1 id="texto_yellow">Search a room</h1>
+					<h1 id="texto_yellow" className="mt-3">
+						Search a room
+					</h1>
 					<br />
+					<br />
+
+					<div className="row">
+						<div className="col-3">
+							<h3 className="filterWords  ml-4">City</h3>
+						</div>
+						<form>
+							<div className="col ml-3">
+								<input type="text" className="form-control roundShape" placeholder="write a city..." />
+							</div>
+						</form>
+					</div>
+
+					<br />
+					<div className="card mx-auto" style={{ width: "70%" }}>
+						<img className="card-img-top roundShape " src={maps} alt="Card image cap" />
+					</div>
+
 					<br />
 					<div>
 						<form>
 							<div className="row">
-								<div className="col-3">
+								<div className="col-4">
 									<h3 className="filterWords ml-4">Price</h3>
 								</div>
-								<div className="col-4">
+								<div className="col-3">
 									<input type="text" className="form-control" placeholder="Min." />
 								</div>
-								<div className="col-4 mr-2">
+								<div className="col-3 mr-2">
 									<input type="text" className="form-control" placeholder="Max." />
+								</div>
+							</div>
+							<br />
+							<div className="row">
+								<div className="col-4">
+									<h3 className="filterWords ml-4">Deposit</h3>
+								</div>
+
+								<div className="col-3">
+									<input type="text" className="form-control" placeholder="Min." />
+								</div>
+								<div className="col-3 mr-2">
+									<input type="text" className="form-control" placeholder="Max." />
+								</div>
+							</div>
+							<br />
+							<br />
+							<div className="row">
+								<div className="col-4">
+									<h3 className="filterWords ml-4">
+										Expenses
+										<br />
+										included
+									</h3>
+								</div>
+								<div className="form-check form-check-inline">
+									<input
+										className="form-check-input"
+										type="radio"
+										name="inlineRadioOptions"
+										id="inlineRadio1"
+										value="option1"
+									/>
+									<label className="form-check-label" htmlFor="inlineRadio1">
+										Wifi
+									</label>
+								</div>
+								<div className="form-check form-check-inline">
+									<input
+										className="form-check-input"
+										type="radio"
+										name="inlineRadioOptions"
+										id="inlineRadio2"
+										value="option2"
+									/>
+									<label className="form-check-label" htmlFor="inlineRadio2">
+										Water
+									</label>
 								</div>
 							</div>
 						</form>
