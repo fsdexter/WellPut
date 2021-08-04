15e694639bf55317724f0788d455bc6df699167b
Jizga <jizga93@gmail.com>
2021-08-04 19:44:32 +0000

    A little style in signUp page

--- src/front/js/pages/signUp.js
+++ src/front/js/pages/signUp.js
@@ -1,12 +1,14 @@
 import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { Context } from "../store/appContext";
-// Bakend URL
-//import { API_BASE_URL } from "../constants";
+
+import "../../styles/signUp.scss";
+import room1 from "../../img/room1.png";
+import room2 from "../../img/room2.png";
+import room3 from "../../img/room3.png";
 
 export const SignUp = () => {
 	const { actions } = useContext(Context);
-	// to go "Edit Profile" after the sign up
 	let history = useHistory();
 	const [formValue, setFormValue] = useState({
 		fullName: "",
@@ -15,7 +17,6 @@ export const SignUp = () => {
 		repearPassword: ""
 	});
 
-	// To get inputs values
 	const inputHandelChange = e => {
 		//"[e.target.name]" is the name of form inputs
 		setFormValue({ ...formValue, [e.target.name]: e.target.value });
@@ -29,59 +30,77 @@ export const SignUp = () => {
 	};
 
 	return (
-		<div className="container row text-center mt-5  d-flex justify-content-center">
-			<div>1</div>
-			<div>2</div>
-			<div>3</div>
-			<form onSubmit={handlerSubmit} className="col-6 p-5 mt-3 myBox text-white">
-				<div className="form-grup row mt-2">
-					<input
-						className="col-6"
-						type="email"
-						name="email"
-						id="email"
-						placeholder="Email"
-						onChange={inputHandelChange}
-						required
-					/>
+		<div className="container row text-center mt-5 d-flex justify-content-center myContainer">
+			<div>
+				<div className="col-6">
+					<h4 className="text text-white">SIGN UP IN</h4>
+					<h1 className="text text-warning">WELL PUT</h1>
 				</div>
-				<div className="form-grup row mt-2">
-					<input
-						className="col-6"
-						type="text"
-						name="fullName"
-						id="fullName"
-						placeholder="Full Name"
-						onChange={inputHandelChange}
-						required
-					/>
+				<div className="row">
+					<div className="col-6 d-flex flex-column">
+						<img src={room1} />
+						<img src={room2} />
+					</div>
+					<div className="col-6" id="room3">
+						<img src={room3} id="imgr3" />
+					</div>
 				</div>
-				<div className="form-grup row mt-2">
-					<input
-						className="col-6"
-						type="password"
-						name="password"
-						placeholder="Password"
-						onChange={inputHandelChange}
-						required
-					/>
-				</div>
-				<div className="form-grup row mt-2">
-					<input
-						className="col-6"
-						type="password"
-						name="repeatPassword"
-						placeholder="Repeat Password"
-						onChange={inputHandelChange}
-						required
-					/>
-				</div>
-				<div>
-					<button type="submit" className="btn btn-outline-warning btn-lg btn-block  mt-4">
-						CONTINUE
-					</button>
-				</div>
-			</form>
+			</div>
+
+			<div className=" col-2">
+				<h4 className="text text-warning">Is too easy</h4>
+			</div>
+			<div className="singUpBox col-4">
+				<form onSubmit={handlerSubmit} className="col-6 p-5 mt-3 myBox text-white">
+					<div className="form-grup row mt-2">
+						<input
+							className="col-6"
+							type="email"
+							name="email"
+							id="email"
+							placeholder="Email"
+							onChange={inputHandelChange}
+							required
+						/>
+					</div>
+					<div className="form-grup row mt-2">
+						<input
+							className="col-6"
+							type="text"
+							name="fullName"
+							id="fullName"
+							placeholder="Full Name"
+							onChange={inputHandelChange}
+							required
+						/>
+					</div>
+					<div className="form-grup row mt-2">
+						<input
+							className="col-6"
+							type="password"
+							name="password"
+							placeholder="Password"
+							onChange={inputHandelChange}
+							required
+						/>
+					</div>
+					<div className="form-grup row mt-2">
+						<input
+							className="col-6"
+							type="password"
+							name="repeatPassword"
+							placeholder="Repeat Password"
+							onChange={inputHandelChange}
+							required
+						/>
+					</div>
+					<div>
+						<button type="submit" className="btn btn-outline-warning btn-lg btn-block  mt-4">
+							CONTINUE
+						</button>
+					</div>
+				</form>
+			</div>
 		</div>
 	);
 };
