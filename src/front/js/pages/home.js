c7f7063687fb437cc4b84907af857338551fdbf9
plotier <plotier15@gmail.com>
2021-08-06 18:07:26 +0000

    features

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -190,6 +190,36 @@ export const Home = () => {
 								<AnimatedMulti /> {/* Instalar paquete para que funcione*/}
 							</div>
 						</div>
+						<br />
+						<div className="row ">
+							<div className="col">
+								<h3 className="ml-4">Features</h3>
+							</div>
+							<div className="form-check form-check-inline col-3 mb-5 pl-4 ">
+								<input
+									className=" form-check-input"
+									type="radio"
+									name="inlineRadioOptions"
+									id="inlineRadio1"
+									value="option1"
+								/>
+								<label className="form-check-label" htmlFor="inlineRadio1">
+									<i className="fas fa-briefcase fa-2x" />
+								</label>
+							</div>
+							<div className="form-check form-check-inline col-3  mb-5 ">
+								<input
+									className="form-check-input"
+									type="radio"
+									name="inlineRadioOptions"
+									id="inlineRadio2"
+									value="option2"
+								/>
+								<label className="form-check-label" htmlFor="inlineRadio2">
+									<i className="fas fa-couch fa-2x" />
+								</label>
+							</div>
+						</div>
 					</div>
 				</div>
 
