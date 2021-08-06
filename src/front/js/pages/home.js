fbae7837565845490e71628d06239f956e73b2e3
plotier <plotier15@gmail.com>
2021-08-06 08:04:30 +0000

    occupation done

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -151,6 +151,44 @@ export const Home = () => {
 								<Counter />
 							</div>
 						</div>
+						<br />
+						<div className="row ">
+							<div className="col">
+								<h3 className="ml-4">Occupation</h3>
+							</div>
+							<div className="form-check form-check-inline col-3 mb-5 pl-4 ">
+								<input
+									className=" form-check-input"
+									type="radio"
+									name="inlineRadioOptions"
+									id="inlineRadio1"
+									value="option1"
+								/>
+								<label className="form-check-label" htmlFor="inlineRadio1">
+									<i className="fas fa-briefcase fa-2x" />
+								</label>
+							</div>
+							<div className="form-check form-check-inline col-3  mb-5 ">
+								<input
+									className="form-check-input"
+									type="radio"
+									name="inlineRadioOptions"
+									id="inlineRadio2"
+									value="option2"
+								/>
+								<label className="form-check-label" htmlFor="inlineRadio2">
+									<i className="fas fa-user-graduate fa-2x" />
+								</label>
+							</div>
+						</div>
+						<div className="row">
+							<div className="col">
+								<h3 className="ml-4">Interests</h3>
+							</div>
+							<div className="pr-5 mr-4 col d-flex">
+								<Rating />
+							</div>
+						</div>
 					</div>
 				</div>
 
