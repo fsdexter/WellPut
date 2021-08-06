29e7db6d8f9a1df7d561b15a73251ee51b4c22a9
plotier <plotier15@gmail.com>
2021-08-06 18:34:37 +0000

    to merge, but not finished

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -204,7 +204,8 @@ export const Home = () => {
 									value="option1"
 								/>
 								<label className="form-check-label" htmlFor="inlineRadio1">
-									<i className="fas fa-briefcase fa-2x" />
+									<i className="fas fa-building fa-2x" />
+									Facing the street
 								</label>
 							</div>
 							<div className="form-check form-check-inline col-3  mb-5 ">
@@ -217,6 +218,37 @@ export const Home = () => {
 								/>
 								<label className="form-check-label" htmlFor="inlineRadio2">
 									<i className="fas fa-couch fa-2x" />
+									Furnished room
+								</label>
+							</div>
+						</div>
+						<br />
+						<div className="row ">
+							<div className="col">
+								<h3 className="ml-4">Bed types</h3>
+							</div>
+							<div className="form-check form-check-inline col-3 mb-5 pl-4 ">
+								<input
+									className=" form-check-input"
+									type="radio"
+									name="inlineRadioOptions"
+									id="inlineRadio1"
+									value="option1"
+								/>
+								<label className="form-check-label" htmlFor="inlineRadio1">
+									Single Bed{" "}
+								</label>
+							</div>
+							<div className="form-check form-check-inline col-3  mb-5 ">
+								<input
+									className="form-check-input"
+									type="radio"
+									name="inlineRadioOptions"
+									id="inlineRadio2"
+									value="option2"
+								/>
+								<label className="form-check-label" htmlFor="inlineRadio2">
+									Double Bed
 								</label>
 							</div>
 						</div>
