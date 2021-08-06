7d80d3ae5d315b8aed0bcb53ffd7282ffe4295b8
plotier <plotier15@gmail.com>
2021-08-06 17:02:44 +0000

    -o fixed

--- src/front/js/component/counter.js
+++ src/front/js/component/counter.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 export function Counter() {
 	const [count, setCount] = useState(1);
+	if (count < 1) {
+		setCount(1);
+	}
 	return (
 		<div className="d-flex">
 			<button className="mr-2 btn btn-default btn-circle" onClick={() => setCount(count - 1)}>
