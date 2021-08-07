4980a649ffbbc6d17d2272424af8f8c2b6360fdd
plotier <plotier15@gmail.com>
2021-08-07 13:57:10 +0000

    all filters, some components

--- src/front/js/component/filterFea.js
+++ src/front/js/component/filterFea.js
@@ -3,7 +3,7 @@ import React from "react";
 export const FilterFea = () => {
 	return (
 		<div className="row ">
-			<div className="col">
+			<div className="col-4">
 				<h3 className="ml-4">Features</h3>
 			</div>
 			<div className="form-check form-check-inline col-3 mb-5 pl-4 ">
@@ -30,6 +30,35 @@ export const FilterFea = () => {
 				<label className="form-check-label" htmlFor="inlineRadio2">
 					<i className="fas fa-couch fa-2x" />
 					Furnished room
+				</label>
+			</div>
+			<div className="col-4" />
+			<div className="form-check form-check-inline col-3  mb-5 pl-4">
+				<input
+					className=" form-check-input"
+					type="radio"
+					name="inlineRadioOptions"
+					id="inlineRadio3"
+					value="option3"
+				/>
+				<label className="form-check-label " htmlFor="inlineRadio3">
+					<i className="fas fa-bath fa-2x" />
+					<br />
+					Suite Room
+				</label>
+			</div>
+			<div className="form-check form-check-inline col-3  mb-5 ">
+				<input
+					className="form-check-input"
+					type="radio"
+					name="inlineRadioOptions"
+					id="inlineRadio4"
+					value="option4"
+				/>
+				<label className="form-check-label" htmlFor="inlineRadio4">
+					<i className="fab fa-slideshare fa-2x" />
+					<br />
+					Shared Room
 				</label>
 			</div>
 		</div>
