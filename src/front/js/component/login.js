488e073d2542ce9f8ae4898da316dabe25ba981b
Jizga <jizga93@gmail.com>
2021-08-07 21:51:26 +0000

    login and signUp fixes

--- src/front/js/component/login.js
+++ src/front/js/component/login.js
@@ -1,61 +1,111 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useRef } from "react";
 import { Context } from "../store/appContext";
-import { useHistory } from "react-router-dom";
+import "../../styles/login.scss";
 
 export const Login = () => {
 	const { actions } = useContext(Context);
-	let history = useHistory();
+	const closeBtn = useRef(null);
 
 	const [formValue, setFormValue] = useState({
 		email: "",
 		password: ""
 	});
 
+	const [errorMsg, setErrorMsg] = useState(null);
+
 	const inputHandelChange = e => {
 		setFormValue({ ...formValue, [e.target.name]: e.target.value });
 	};
 
-	const handlerSubmit = e => {
+	const handlerSubmit = async e => {
 		e.preventDefault();
 		// login function
-		actions.login(formValue);
+		let loginError = await actions.login(formValue);
+
+		if (loginError) {
+			setErrorMsg(loginError);
+		} else {
+			closeBtn.current.click();
+		}
 	};
 
-	// to go home after the sign up
-	if (actions.isUserAuthentificted()) {
-		history.push("/");
-	}
-
 	return (
-		<div className="container row text-center mt-5  d-flex justify-content-center">
-			<form onSubmit={handlerSubmit} className="col-6 p-5 mt-3 myBox text-white">
-				<div className="form-grup row mt-2">
-					<input
-						className="col-6"
-						type="email"
-						name="email"
-						id="email"
-						placeholder="Email"
-						onChange={inputHandelChange}
-						required
-					/>
+		<div className="row container text-center d-flex justify-content-around" id="loginContainer">
+			<div className="col-4" id="loginImg">
+				<div className="onLoginRoom pl-3 pt-5">
+					<h1 className="text fs-1 myYellowText ml-2 well">WELL</h1>
+					<h1 className="text fs-1 myYellowText ml-2 put custom-ml">PUT</h1>
+					<h4 className="text text-white mt-5">
+						<span className="myYellowText">We help you</span> find the ideal place for you!
+					</h4>
+					<h4 className="text text-white mt-3">
+						<span className="myYellowText">A perfect space</span> with suitable companions ...
+					</h4>
 				</div>
-				<div className="form-grup row mt-2">
-					<input
-						className="col-6"
-						type="password"
-						name="password"
-						placeholder="Password"
-						onChange={inputHandelChange}
-						required
-					/>
+			</div>
+			<div className="col-6 mt-2">
+				<div
+					className="iconClose mt-2 m-0 p-0 d-flex justify-content-end close"
+					data-dismiss="modal"
+					ref={closeBtn}>
+					<i className="far fa-window-close text-white fa-lg" />
 				</div>
-				<div>
-					<button type="submit" className="btn btn-outline-primary btn-lg btn-block  mt-4">
-						Login
+				{errorMsg ? (
+					<div className="alert alert-danger alert-dismissible fade show" role="alert">
+						{errorMsg}
+						<button
+							type="button"
+							className="close"
+							data-dismiss="alert"
+							onClick={() => setErrorMsg(null)}
+							aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+				) : null}
+
+				<form onSubmit={handlerSubmit} className="col-12 p-5 mt-5 myBox text-white" id="loginForm">
+					<div className="form-grup row mt-2">
+						<input
+							className="col-12"
+							type="email"
+							name="email"
+							id="email"
+							placeholder="Email"
+							onChange={inputHandelChange}
+							required
+						/>
+					</div>
+					<div className="form-grup row mt-4">
+						<input
+							className="col-12"
+							type="password"
+							name="password"
+							placeholder="Password"
+							onChange={inputHandelChange}
+							required
+						/>
+					</div>
+					<p className="recoverPass">Recover password</p>
+
+					<button type="submit" className="btn btnYellow mt-4">
+						LOGIN
+					</button>
+				</form>
+				<div className="signUpButton pb-5">
+					<div className="d-flex flex-column create-acount">
+						<span>Don’t have an account?</span>
+						<span>create one here</span>
+					</div>
+					<button
+						className="btn btnYellow"
+						data-dismiss="modal"
+						data-toggle="modal"
+						data-target="#signUpModal">
+						SIGN UP
 					</button>
 				</div>
-			</form>
+			</div>
 		</div>
 	);
 };
--- src/front/js/component/signUp.js
+++ src/front/js/component/signUp.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useRef } from "react";
 import { useHistory } from "react-router-dom";
 import { Context } from "../store/appContext";
 
@@ -8,8 +8,9 @@ import room2 from "../../img/room2.png";
 import room3 from "../../img/room3.png";
 
 export const SignUp = () => {
-	const { actions } = useContext(Context);
-	let history = useHistory();
+	const { store, actions } = useContext(Context);
+	const history = useHistory();
+	const closeBtn = useRef(null);
 	const [formValue, setFormValue] = useState({
 		fullName: "",
 		email: "",
@@ -22,40 +23,37 @@ export const SignUp = () => {
 		setFormValue({ ...formValue, [e.target.name]: e.target.value });
 	};
 
-	const handlerSubmit = e => {
+	const handlerSubmit = async e => {
 		e.preventDefault();
-		actions.signUp(formValue);
-		// to go "Edit Profile" after the sign up
-		history.push("/edit_profile");
+		const signUpError = await actions.signUp(formValue);
+		closeBtn.current.click();
+		// to go "Edit Profile" after the correct sign up
+		if (!signUpError) {
+			history.push("/edit_profile");
+		}
 	};
 
 	return (
 		<div className="row container text-center d-flex justify-content-center" id="myContainer">
 			<div className="col-3 no-gutters">
-				<div className="col-8">
-					<h4 className="text text-white" id="myTitle">
-						SIGN UP IN
-					</h4>
-					<h1 className="text fs-1 myYellowText" id="well">
-						WELL
-					</h1>
-					<h1 className="text fs-1 myYellowText" id="put">
-						PUT
-					</h1>
+				<div className="col-8 textSignUp">
+					<h4 className="text text-white">SIGN UP IN</h4>
+					<h1 className="text fs-1 myYellowText well">WELL</h1>
+					<h1 className="text fs-1 myYellowText put">PUT</h1>
 				</div>
 				<div className="col-12" id="imgFlower">
 					<div className="row p-0 pb-1 m-0">
 						<div className="col-6 p-0 d-flex justify-content-end">
-							<img src={room1} />
+							<img src={room1} className="signUpRoom" />
 						</div>
 						<div className="col-6 p-0 d-flex justify-content-start" />
 					</div>
 					<div className="row p-0 m-0">
 						<div className="col-6 p-0 pr-1 d-flex justify-content-end">
-							<img src={room2} />
+							<img src={room2} className="signUpRoom" />
 						</div>
 						<div className="col-6 ml-0 p-0 pl-1 d-flex justify-content-start">
-							<img src={room3} />
+							<img src={room3} className="signUpRoom" />
 						</div>
 					</div>
 				</div>
@@ -112,7 +110,7 @@ export const SignUp = () => {
 						/>
 					</div>
 					<div>
-						<button type="submit" className="btn btnYellow mt-4 ">
+						<button type="submit" className="btn btnYellow mt-4">
 							CONTINUE
 						</button>
 					</div>
@@ -120,7 +118,8 @@ export const SignUp = () => {
 			</div>
 			<div
 				className="col-1 no-gutters iconClose mt-2 m-0 p-0 d-flex justify-content-end close"
-				data-dismiss="modal">
+				data-dismiss="modal"
+				ref={closeBtn}>
 				<i className="far fa-window-close text-white fa-lg" />
 			</div>
 		</div>
