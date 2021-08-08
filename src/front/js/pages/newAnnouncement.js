0cb268b9d7100fe21ded3fafab40b83fb781ad9f
plotier <plotier15@gmail.com>
2021-08-08 19:30:31 +0000

    expenses done in newAnnouncement

--- src/front/js/pages/newAnnouncement.js
+++ src/front/js/pages/newAnnouncement.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/newAnnouncement.scss";
 import maps from "../../img/maps.png";
+import { FilterExp } from "../component/filterExp";
 
 export const NewAnnouncement = () => {
 	const { store, actions } = useContext(Context);
@@ -120,7 +121,7 @@ export const NewAnnouncement = () => {
 						<div className="col-1" />
 					</div>
 					<div className="row  mt-2 ">
-						<div className="col-6">
+						<div className="col">
 							<div className="row ">
 								<div className="col-4">
 									<p className="fontInput pl-2">Price</p>{" "}
@@ -144,7 +145,66 @@ export const NewAnnouncement = () => {
 								</div>
 							</div>
 						</div>
-						<div className="col-6" />
+						<div className="col">
+							<div className="row">
+								<div className="col-4">
+									<p className="fontInput">
+										Expenses
+										<br />
+										included
+									</p>
+								</div>
+								<div className="form-check form-check-inline col-3 pb-5 ">
+									<input
+										className=" form-check-input"
+										type="checkbox"
+										name="inlineRadioOptions"
+										id="inlineRadio1"
+										value="option1"
+									/>
+									<label className="form-check-label" htmlFor="inlineRadio1">
+										<i className="fas fa-wifi fa-2x" />
+									</label>
+								</div>
+								<div className="form-check form-check-inline col-3 pb-5 ">
+									<input
+										className="form-check-input"
+										type="checkbox"
+										name="inlineRadioOptions"
+										id="inlineRadio2"
+										value="option2"
+									/>
+									<label className="form-check-label" htmlFor="inlineRadio2">
+										<i className="fas fa-shower fa-2x" />
+									</label>
+								</div>
+								<div className="col-4" />
+								<div className="form-check form-check-inline col-3  pb-5 ">
+									<input
+										className=" form-check-input"
+										type="checkbox"
+										name="inlineRadioOptions"
+										id="inlineRadio3"
+										value="option3"
+									/>
+									<label className="form-check-label " htmlFor="inlineRadio3">
+										<i className="fas fa-burn fa-2x  " />
+									</label>
+								</div>
+								<div className="form-check form-check-inline col-3 pb-5 ">
+									<input
+										className="form-check-input"
+										type="checkbox"
+										name="inlineRadioOptions"
+										id="inlineRadio4"
+										value="option4"
+									/>
+									<label className="form-check-label" htmlFor="inlineRadio4">
+										<i className="far fa-lightbulb fa-2x " />
+									</label>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 				<div className="tab-pane fade" id="picsTab" role="tabpanel" aria-labelledby="picsTab-tab">
