cfd4cb7da5847e6ca59cbfef18d58d831e9d9bbb
plotier <plotier15@gmail.com>
2021-08-11 10:06:20 +0000

    open eye button done

--- src/front/js/component/myRoomsItem.js
+++ src/front/js/component/myRoomsItem.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "../../styles/announcements.scss";
+import openEye from "../../img/openEye.png";
 
 export const MyRoomsItemActive = () => {
 	return (
 		<div className="row">
 			<div className="third_part mx-auto mt-3 mb-2">
 				<div className="row">
-					<div className="col-10">10 aaaaaaa</div>
-					<div className="col-2">2 aaaaaaa</div>
+					<div className="col-6">9 aaaaaaa</div>
+					<div className="col-4 roomItemBar">2 aaaaaaa</div>
+					<div className="col-2">
+						<div className="eyeButton">
+							<button type="button" className="btn btn-outline-dark">
+								<img src={openEye} />
+							</button>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -18,8 +26,9 @@ export const MyRoomsItemInactive = () => {
 		<div className="row">
 			<div className="third_part mx-auto mt-3 mb-2">
 				<div className="row">
-					<div className="col-10">10 iiiiiii</div>
-					<div className="col-2">2 iiiiiiii</div>
+					<div className="col-9">9 iiiiiiiiiii</div>
+					<div className="col-2 roomItemBar">2 iiiiiii</div>
+					<div className="col-1">1 iiii</div>
 				</div>
 			</div>
 		</div>
@@ -30,8 +39,9 @@ export const MyRoomsItemOccupied = () => {
 		<div className="row">
 			<div className="third_part mx-auto mt-3 mb-2">
 				<div className="row">
-					<div className="col-10">10 oooooooooo</div>
-					<div className="col-2">2 oooooooo</div>
+					<div className="col-9">9 ooooo</div>
+					<div className="col-2 roomItemBar">2ooooo</div>
+					<div className="col-1">1ooo</div>
 				</div>
 			</div>
 		</div>
