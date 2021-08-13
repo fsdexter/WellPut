53fbf5a771382f7448d7b28bc6525cf17fbb8f39
Naiceiry <naiceirymp@gmail.com>
2021-08-13 17:17:13 +0000

    inicio

--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -1,13 +1,15 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import "../../styles/home.scss";
-
+import "../../styles/viewprofile.scss";
+import firtsimgprofile from "../../img/photoprofile.png";
 export const Profile = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Profile page</h1>
+		<div className="text-center mt-5 picturefond">
+			<div className="text-center mt-5 pictureprofile1">
+				<img className="card-img-top roundShape " src={firtsimgprofile} alt="Card image cap" />
+			</div>
 		</div>
 	);
 };
