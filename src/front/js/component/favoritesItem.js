211819a982774f7f80a3563547c5c06d315a9145
plotier <plotier15@gmail.com>
2021-08-13 08:49:50 +0000

    title done

--- src/front/js/component/favoritesItem.js
+++ src/front/js/component/favoritesItem.js
@@ -5,13 +5,20 @@ import room from "../../img/room.jpg";
 import deleteRoom from "../../img/deleteRoom.png";
 
 const FavPriceExample = 450;
+const FavTitleExample = "Habitación luminosa frente a Sagrada Familia";
 
 export const FavoritesItem = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="row mt-2 pb-2 border-bottom border-dark">
-			<div className="col-6">Info</div>
+			<div className="col-6">
+				<div className="row">
+					<h5 className="ml-2 mt-2 favTitle pl-3">{FavTitleExample}</h5>
+				</div>
+				<div className="row">2</div>
+				<div className="row">3</div>
+			</div>
 			<div className="col-2">
 				<h1 className="favPrice">€{FavPriceExample}</h1>
 			</div>
