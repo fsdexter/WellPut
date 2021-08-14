ff063e3bf735e9c55a3e4c608a9b179ea4f62794
plotier <plotier15@gmail.com>
2021-08-14 09:20:47 +0000

    pic resize fixed

--- src/front/js/component/carouselRoomImg.js
+++ src/front/js/component/carouselRoomImg.js
@@ -2,7 +2,9 @@ import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 // Librería para el carousel
 import AwesomeSlider from "react-awesome-slider";
-import "react-awesome-slider/dist/styles.css";
+import "react-awesome-slider/dist/styles.css?raw";
+import "../../styles/carouselRoomImg.scss";
+import { RatingStatic } from "./ratingStatic";
 
 import roomDetails1 from "../../img/roomDetails.png";
 import roomDetails2 from "../../img/cama.png";
@@ -14,16 +16,33 @@ export const CarouselRoomImg = () => {
 	const { store, actions } = useContext(Context);
 	const slider = (
 		<div>
-			<AwesomeSlider>
-				{/* Hacer cuando tengamos datos de habitaciones en el flux*/}
-				{/* {store.roomList.map( room => {
-				<div key={room.id} data-src={room.photo_url} className="carouselimgRoom"/>
-			})} */}
+			<div id="carouselExampleControls" className="carousel slide" data-ride="carousel" data-interval="false">
+				<div className="carousel-inner caroShape d-flex">
+					<div className="carousel-item  active ">
+						<img className="d-block w-100 caro_pic_fix" src={roomDetails1} alt="First slide" />
 
-				<div data-src={roomDetails1} className="carouselimgRoom" />
-				<div data-src={roomDetails2} className="carouselimgRoom" />
-				<div data-src={roomDetails3} className="carouselimgRoom" />
-			</AwesomeSlider>
+						<div className="carousel-caption ">
+							<RatingStatic />
+						</div>
+					</div>
+					<div className="carousel-item  ">
+						<img className="d-block w-100 caro_pic_fix" src={roomDetails2} alt="Second slide" />
+						<div className="carousel-caption ">sdfsdfsdfsdf</div>
+					</div>
+					<div className="carousel-item ">
+						<img className="d-block w-100 caro_pic_fix" src={roomDetails3} alt="Third slide" />
+						<div className="carousel-caption">sdfsdfsdfsdf</div>
+					</div>
+				</div>
+				<a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+					<span className="carousel-control-prev-icon" aria-hidden="true" />
+					<span className="sr-only">Previous</span>
+				</a>
+				<a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+					<span className="carousel-control-next-icon" aria-hidden="true" />
+					<span className="sr-only">Next</span>
+				</a>
+			</div>
 		</div>
 	);
 
