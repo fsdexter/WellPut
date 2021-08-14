f8b25d822da4b25a5fd3e6e214237fecaa605a2c
Jizga <jizga93@gmail.com>
2021-08-14 11:30:42 +0000

    OwnerResume component done

--- src/front/js/component/ownerResume.js
+++ src/front/js/component/ownerResume.js
@@ -1,13 +1,25 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+
+import ownerImg from "../../img/mia.png";
 import "../../styles/detailedView.scss";
 
 export const OwnerResume = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>OWNER RESUME</h1>
+		<div className="d-flex flex-column">
+			<div className="row d-flex justify-content-end">
+				<img src={ownerImg} className="col-10" />
+			</div>
+			<div className="row d-flex justify-content-end">
+				<div className="col-10 d-flex flex-column p-4 mt-2" id="ownRes">
+					<p className="text-white p-3">Cheerful, sociable and animal-loving girl</p>
+					<button className="btn btnYellow mt-4" id="btnYeOwnR">
+						Know more about Mía
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/front/js/pages/detailedView.js
+++ src/front/js/pages/detailedView.js
@@ -5,22 +5,57 @@ import { OwnerResume } from "../component/ownerResume";
 import { ReviewsResume } from "../component/reviewsResumen";
 import { CarouselRoomImg } from "../component/carouselRoomImg";
 
+import roomDetails1 from "../../img/roomDetails.png";
+
 import "../../styles/detailedView.scss";
 
 export const DetailedView = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="container">
-			<div className="row d-flex justify-content-center bg-primary">
-				<div className="col-10 ">
-					<CarouselRoomImg />
-				</div>
-			</div>
-
-			<div className="row bg-danger">
+		<div className="row d-flex justify-content-around">
+			<div className="col-2 align-self-end mb-3">
 				<OwnerResume />
-				<ReviewsResume />
+			</div>
+			<div className="col-9 mr-2 mb-3">
+				<div className="row d-flex justify-content-center mb-2">
+					<img src={roomDetails1} className="col-12" />
+				</div>
+				<di className="row d-flex justify-content-center mb-3 mt-3">
+					<div className="col-12 d-flex flex-column">
+						<h5 className="font-weight-bolder"> Description</h5>
+						<p>
+							Exterior room with double bed and private bathroom in the Salamanca district, very bright,
+							fully equipped and with all the comforts. Young and calm environment, NO SMOKING, NO
+							COUPLES. Unbeatable location near the metro, bus and other local shops.
+						</p>
+					</div>
+				</di>
+				<div className="row d-flex justify-content-around">
+					<div className="col-2 d-flex flex-column">
+						<div className="d-flex flex-column mb-2">
+							<h5 className="font-weight-bolder"> Expenses included</h5>
+							<div className="d-flex justify-content-around">
+								<i className="fas fa-wifi fa-2x" />
+								<i className="fas fa-shower fa-2x" />
+								<i className="fas fa-burn fa-2x  " />
+								<i className="far fa-lightbulb fa-2x " />
+							</div>
+						</div>
+						<div className="d-flex flex-column mt-1">
+							<h5 className="font-weight-bolder">Features</h5>
+							<div className="d-flex justify-content-around">
+								<i className="fas fa-building fa-2x" />
+								<i className="fas fa-couch fa-2x" />
+								<i className="fas fa-bath fa-2x" />
+							</div>
+						</div>
+					</div>
+					<div className="col-2 bg-warning">ENLACE PERFIL DE INQUILINOS</div>
+					<div className="col-5 bg-info">
+						<ReviewsResume />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
