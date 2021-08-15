093148faa5e1ee052b9d5226cccd6cdd5180881f
Naiceiry <naiceirymp@gmail.com>
2021-08-15 13:57:01 +0000

    nada

--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -2,24 +2,43 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/viewprofile.scss";
 import firtsimgprofile from "../../img/photoprofile.png";
+import argentina from "../../img/argentina.png";
 export const Profile = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="picturefond">
-			<div className="container col-lg-12 detallefondblack">
+		<div className="picturefond col-lg-12">
+			<div className="container col-lg-9 detallefondblack">
 				<div className="row">
-					<div className="col">
-						<img
-							className="card-img-top roundShape col-lg-7´´"
-							src={firtsimgprofile}
-							alt="Card image cap"
-						/>
-					</div>
-
-					<div className="col">
-						<div className="row">1 of 3</div>
-						<div className="row">1 of 3</div>
+					<img className="card-img-top roundShape col-lg-4" src={firtsimgprofile} alt="Card image cap" />
+					<div className="block">
+						<div className="col-12">
+							<div className="row-9">
+								<img
+									className="card-img-top roundShape"
+									src={argentina}
+									alt="Card image cap"
+									style={{ width: "2px", height: "78px" }}
+								/>
+							</div>
+							<div className="row-2">
+								<h1>Jason Becker-29</h1>
+							</div>
+							<div className="row-3">
+								<i className="fa fa-star start" aria-hidden="true" />
+								&nbsp;&nbsp;
+								<i className="fa fa-star start" aria-hidden="true" />
+								&nbsp;&nbsp;
+								<i className="fa fa-star start" aria-hidden="true" />
+								&nbsp;&nbsp;
+								<i className="fa fa-star start" aria-hidden="true" />
+								&nbsp;&nbsp;
+								<i className="fa fa-star start" aria-hidden="true" />
+							</div>
+						</div>
+						<div className="row">
+							<div className="col-12">aqui va la descripcop</div>
+						</div>
 					</div>
 				</div>
 			</div>
