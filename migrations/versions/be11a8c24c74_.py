40762b572eb63cd168cad275d90fdf298e64a9aa
Jizga <jizga93@gmail.com>
2021-09-16 18:42:05 +0000

    migraciones BD

--- migrations/versions/be11a8c24c74_.py
+++ migrations/versions/be11a8c24c74_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 636b10988853
+Revision ID: be11a8c24c74
 Revises: 
-Create Date: 2021-09-16 18:24:18.969527
+Create Date: 2021-09-16 18:39:16.414905
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '636b10988853'
+revision = 'be11a8c24c74'
 down_revision = None
 branch_labels = None
 depends_on = None
