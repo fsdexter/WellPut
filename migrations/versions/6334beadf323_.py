cac062a18a538f7261c4cc938eef44102564d643
Jizga <jizga93@gmail.com>
2021-10-10 19:25:58 +0000

    migraciones

--- migrations/versions/6334beadf323_.py
+++ migrations/versions/6334beadf323_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 8d98072afba9
+Revision ID: 6334beadf323
 Revises: 
-Create Date: 2021-10-10 17:25:23.691623
+Create Date: 2021-10-10 18:33:14.870837
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '8d98072afba9'
+revision = '6334beadf323'
 down_revision = None
 branch_labels = None
 depends_on = None
