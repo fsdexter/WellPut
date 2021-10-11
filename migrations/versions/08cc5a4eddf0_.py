3ddd3bf4a4b87b70b01c43c562f701dc62d03e6a
Jizga <jizga93@gmail.com>
2021-10-11 16:31:26 +0000

    ultimo cambio

--- migrations/versions/08cc5a4eddf0_.py
+++ migrations/versions/08cc5a4eddf0_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: d5b3241b0c38
+Revision ID: 08cc5a4eddf0
 Revises: 
-Create Date: 2021-10-11 15:45:28.900727
+Create Date: 2021-10-11 16:22:30.559182
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'd5b3241b0c38'
+revision = '08cc5a4eddf0'
 down_revision = None
 branch_labels = None
 depends_on = None
